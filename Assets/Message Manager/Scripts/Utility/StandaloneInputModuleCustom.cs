2fe848894f329f4c9f1902a326cf0e5468c1f579
RobTen <rob@tenpoundsledge.com>
2019-02-08 18:19:33 -0700

    Renamed some game objects for clarity.

--- Assets/Message Manager/Scripts/Utility/StandaloneInputModuleCustom.cs
+++ Assets/Message Manager/Scripts/Utility/StandaloneInputModuleCustom.cs
@@ -3,9 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+//NOTE: This probably isn't entirely necessary but it works well.
 public class StandaloneInputModuleCustom : StandaloneInputModule
 {
-
     public PointerEventData GetLastPointerEventDataPublic(int id)
     {
         return GetLastPointerEventData(id);
